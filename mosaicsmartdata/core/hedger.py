616328e5b8a882c77859ccc6803139a2a4f306d2
Sumit Sengupta <sumit@mosaicsmartdata.com>
2017-03-27 20:35:12 +0100

    adding initial hedge logic

--- mosaicsmartdata/core/hedger.py
+++ mosaicsmartdata/core/hedger.py
@@ -1,5 +1,7 @@
 import copy
-from mosaicsmartdata.core.trade import Quote,Trade
+from mosaicsmartdata.common.constants import *
+from mosaicsmartdata.common.read_config import *
+from mosaicsmartdata.core.trade import Quote, Trade, FixedIncomeHedge
 from mosaicsmartdata.common.constants import *
 
 
@@ -22,14 +24,12 @@ class Hedger:
         if isinstance(msg, Quote):
             # store the latest quote for each instrument;
             # make a copy just in case the msg object gets modififed downstream
-            self.last_quotes[msg.sym] = copy.deepcopy(msg)
-
+            self.last_quotes[msg.sym] = msg  # copy.deepcopy(msg)
             return []
 
         elif isinstance(msg, Trade):
             hedges = self.hedge_calculator(msg, self.last_quotes)
-
-            return hedges + [msg]
+            return hedges
 
         else:
             raise ValueError('Message must be a subclass of either Quote or Trade!')
@@ -38,13 +38,44 @@ class Hedger:
 def my_hedge_calculator(msg, lastquotes):
     # as a simple example, just do opposite trade as a hedge
     hedge_dict = msg.__dict__
+    hedge_mapper = load_config(msg.ccy)
 
-    if msg.side == TradeSide.Bid:
-        hedge_dict.side == TradeSide.Ask
+    min_hedge_delta = float(hedge_mapper['min_hedge_delta'])
+
+    # extract the tenors - first 2 config are hedge specific different config
+    tenors_list = sorted([float(x) for x in (sorted([x for x in list(hedge_mapper.keys())])[:-2])])
+    # get the hedge(s)
+    hedge_arr = hedge_mapper[str([x for x in tenors_list if msg.tenor > x][-1]).rstrip('0').rstrip('.')]
+    # set the duration of the hedge based on the Quote object
+    if hedge_arr not in lastquotes:
+        raise ValueError('No Hedge [rices received before receiving a Trade object!')
     else:
-        hedge_dict.side == TradeSide.Bid
+        hedge_quote = lastquotes[hedge_arr]
 
-    hedge_dict.trade_id = 'hedge_1_' + msg.trade_id
-    hedge_dict.paper_trade = True
+    hedge_trade = FixedIncomeHedge(trade_id=msg.trade_id + "_HEDGE",
+                                   sym=hedge_arr,
+                                   paper_trade=True,
+                                   notional=1,
+                                   delta=None,
+                                   timestamp=msg.timestamp,
+                                   side=TradeSide.Ask if msg.side == TradeSide.Bid else TradeSide.Bid,
+                                   traded_px=hedge_quote.ask if msg.side == TradeSide.Ask else TradeSide.Bid,
+                                   client_sys_key=msg.client_sys_key,
+                                   trade_date=hedge_quote.timestamp.date(),
+                                   ccy=msg.ccy,
+                                   is_cash = True,
+                                   is_futures=False,
+                                   trade_settle_date=None,
+                                   min_hedge_delta=min_hedge_delta,
+                                   trade = msg,
+                                   duration=hedge_quote.duration)
 
-    return msg.__class__(**hedge_dict)
+    return hedge_trade
+
+
+def load_config(ccy):
+    if ccy == Currency.USD:
+        # load the USD relevant hedge config
+        return get_data_given_section('USD_GovtBond_Hedge_Mapper')
+    else:
+        ValueError('Not implemented')
--- mosaicsmartdata/core/trade.py
+++ mosaicsmartdata/core/trade.py
@@ -1,6 +1,8 @@
 import datetime as dt
-
+import numpy as np
+from mosaicsmartdata.common import read_config
 from mosaicsmartdata.common.constants import *
+
 
 class GenericParent:
     def __init__(self, *args, **kwargs):
@@ -37,6 +39,7 @@ def to_dict(x):
     my_dict['_class'] = str(type(x)).replace('\'', '').replace('>', '').split('.')[-1]
     return my_dict
 
+
 # reflate the class back from a dict
 def from_dict(x):
     atype = globals()[x.pop('_class')]
@@ -49,7 +52,7 @@ class Quote(GenericParent):
         self.timestamp = None  # <-- should be in datetime format
         self.bid = None
         self.ask = None
-        self.duration = None # <-- need this for hedging
+        self.duration = None  # <-- need this for hedging
 
         # just paste this magic line in to assign the kwargs
         super().__init__(**(self.apply_kwargs(self.__dict__, kwargs)))
@@ -63,9 +66,10 @@ class Quote(GenericParent):
             return self.__dict__[item]
         # calculate different markout types on the fly by applying the correct multiplier
         elif str(item) == 'mid':
-            return 0.5*(self.bid + self.ask)
+            return 0.5 * (self.bid + self.ask)
         else:
-            raise ValueError('This object doesn\'t have attribute \"' + item + '\"' )
+            if str(item) not in ['__deepcopy__', '__getstate__']:
+                raise ValueError('This object doesn\'t have attribute \"' + item + '\"')
 
 
 class Trade(GenericParent):
@@ -80,13 +84,13 @@ class Trade(GenericParent):
         self.traded_px = None
         self.trade_date = None
         self.trade_settle_date = None
-        self.ccy=Currency.EUR
+        self.ccy = Currency.EUR
         self.client_sys_key = None
         self.trade_rc = None
         self.sales = None
         self.trader = None
         self.delta = None
-
+        self.tenor = None
         # just paste this magic line in to assign the kwargs
         super().__init__(**(self.apply_kwargs(self.__dict__, kwargs)))
 
@@ -98,6 +102,7 @@ class Trade(GenericParent):
 
         def markout_mults(self):
             return {'price': 1, 'PV': self.delta}
+
 
 class FixedIncomeTrade(Trade):
     # Attributes with default parameter.
@@ -117,6 +122,7 @@ class FixedIncomeTrade(Trade):
         self.trade_added_to_rp = False
         self.is_d2d_force_close = False
         self.duration = None
+        self.beta = None  # for hedging using multiple assets
         self.par_value = 100
         self.spot_settle_date = None
         self.issue_date = None
@@ -131,12 +137,64 @@ class FixedIncomeTrade(Trade):
         if self.delta is None:
             self.delta = self.duration * self.notional * 0.0001
 
-
     def markout_mults(self):
         return {'price': 1,
                 'PV': self.delta,
                 'cents': 100.0,
-                'bps':(1.0 / self.duration / self.par_value) * 10000}
+                'bps': (1.0 / self.duration / self.par_value) * 10000}
+
+    def __str__(self):
+        my_string = str(type(self)) + ' : '
+        for key, value in self.__dict__.items():
+            my_string = my_string + ', ' + key + ' = ' + str(value)
+        my_string = my_string.replace(': ,', ': ')
+        return my_string
+
+class FixedIncomeHedge(Trade):
+    def __init__(self, *args, **kwargs):
+        self.package_id = None
+
+        self.is_futures = False
+        self.is_cash = False
+        self.min_hedge_delta = 1000  # in relevant ccy
+        self.trade = None
+        self.duration = None
+        super().__init__(**(self.apply_kwargs(self.__dict__, kwargs)))
+        self.notional = 1  # for futures
+        self.paper_trade = True
+        self.hedge_contracts = 0
+        self.hedge_cost = 0.0
+        if self.delta is None and not self.is_futures:
+            # futures delta should always be passed in
+            self.delta = self.duration * self.notional * 0.0001
+        self.package_id = self.trade.package_id
+        self.calculate_hedge_contracts()
+        self.calculate_initial_hedge_cost()
+
+    def calculate_hedge_contracts(self):
+        if self.trade.delta > self.min_hedge_delta:
+            if self.trade.beta is None:
+                self.trade.beta = 1  # todo: need to add beta functionality
+            if self.is_futures:
+                # we expect the futures delta to be passed in per 100000 notional
+                self.hedge_contracts = np.round(self.trade.beta * self.trade.delta / self.delta, 0)
+                if self.notional is None:
+                    self.notional = 100000
+            else:
+                # for Cash, this will simply be the notional of the hedge bond
+                self.hedge_contracts = self.trade.beta * self.trade.delta / self.delta
+
+        else:
+            self.hedge_contracts = 0.0
+
+    def calculate_initial_hedge_cost(self, contracts=None):
+        if self.hedge_contracts is None:
+            self.calculate_hedge_contracts()
+        if self.hedge_contracts > 0:
+            self.hedge_cost = self.hedge_contracts * self.notional
+        else:
+            self.hedge_cost = 0.0
+
 
 if __name__ == '__main__':
     date_0 = dt.datetime(2017, 1, 2)
