52f7d7cd12b80234ac8f2daa098e1365600c7ec2
Sumit Sengupta <sumit@mosaicsmartdata.com>
2017-03-29 16:16:51 +0100

    added handling of NaN for empty markouts

--- mosaicsmartdata/unit_tests/test_markout.py
+++ mosaicsmartdata/unit_tests/test_markout.py
@@ -301,3 +301,55 @@ class TestMarkouts(TestCase):
             elif mk_msg.trade_id == "UST30Y_OTR_111111" and mk_msg.dt == 'COB2':
                 self.assertLessEqual(np.abs((mk_msg.bps_markout - (-13.41)) / mk_msg.bps_markout), tolerance,
                                      msg=None)
+
+    # test NaN
+    def test_case_6(self, plotFigure=False):
+        tolerance = 5 * 1e-2
+        datapath = "..\\resources\\"  # generally a good idea to use relative paths whenever possible
+        quote_files = ["912810QE1_quotes.csv"]
+        trade_files = "trades_NaN_test.csv"
+
+        # Load the quotes data from csv
+        quotes_dict = dict()
+        for x in quote_files:
+            sym, quote = qc_csv_helper.file_to_quote_list(datapath + x, MarkoutMode.Unhedged)
+            quotes_dict[sym] = quote
+
+        # Now get the trades list from csv
+        trades_list = qc_csv_helper.file_to_trade_list(datapath + trade_files)
+
+        # Method 2 - go through each of the instruments,
+        # create a stream of quote per sym
+        quote_trade_list = []
+        for k, v in quotes_dict.items():
+            # quote_async_iter = to_async_iterable(quotes_dict[k])
+            # quote_trade_list.append(quote_async_iter)
+            quote_async_iter = to_async_iterable(quotes_dict[k])
+            # trades_list_sym = []
+            # [trades_list_sym.append(t) for t in trades_list if t.sym == k]
+
+            # trade_async_iter = to_async_iterable(trades_list_sym)
+            quote_trade_list.append(quote_async_iter)
+            # quote_trade_list.append(trade_async_iter)
+
+        output_list = []
+        trade_async_iter = to_async_iterable(trades_list)
+        quote_trade_list.append(trade_async_iter)
+        joint_stream = op.merge_sorted(quote_trade_list, lambda x: x.timestamp)
+        joint_stream | op.map_by_group(lambda x: x.sym, GovtBondMarkoutCalculator()) | op.flatten() > output_list
+
+        # do assertions
+        self.assertEquals(len(output_list), 6, msg=None)
+        for mk_msg in output_list:
+            if mk_msg.trade_id == "222" and mk_msg.dt == '-900':
+                self.assertEquals(mk_msg.bps_markout, "NaN", msg=None)
+            elif mk_msg.trade_id == "222" and mk_msg.dt == '-60':
+                self.assertEquals(mk_msg.bps_markout, "NaN", msg=None)
+            elif mk_msg.trade_id == "222" and mk_msg.dt == '0':
+                self.assertEquals(mk_msg.bps_markout, 0.0, msg=None)
+            elif mk_msg.trade_id == "222" and mk_msg.dt == '60':
+                self.assertEquals(mk_msg.bps_markout, 0.0, msg=None)
+            elif mk_msg.trade_id == "222" and mk_msg.dt == '300':
+                self.assertLessEqual(np.abs((mk_msg.bps_markout - (-0.2439)) / mk_msg.bps_markout), tolerance, msg=None)
+            elif mk_msg.trade_id == "222" and mk_msg.dt == '3600':
+                self.assertLessEqual(np.abs(mk_msg.bps_markout - 0.4873) / mk_msg.bps_markout, tolerance, msg=None)
