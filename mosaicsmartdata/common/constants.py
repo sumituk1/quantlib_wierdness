f275a8c6cee65ec46b32bfb92de5917abb6b461c
Sumit Sengupta <sumit@mosaicsmartdata.com>
2017-04-04 11:39:34 +0100

    adding futures hedge logic

--- mosaicsmartdata/common/constants.py
+++ mosaicsmartdata/common/constants.py
@@ -6,9 +6,11 @@ class BootStrapMethod:
     PiecewiseLogCubicDiscount = "PicewiseLogCubicDiscount"
     PiecewiseFlatForward = "PiecewiseFlatForward"
 
+
 class MarkoutMode:
     Unhedged = 1
     Hedged = 2
+
 
 class DayCountConv:
     ACT_ACT = "ACT/ACT"
@@ -16,12 +18,12 @@ class DayCountConv:
     ACT_360 = "ACT/360"
     ACT_365 = "ACT/365"
     ACT_365L = "ACT/365L"
-    ACT_252 = "ACT/252" # -> Business252 in Quantlib
+    ACT_252 = "ACT/252"  # -> Business252 in Quantlib
 
     BASIC30_360 = "BASIC30/360"
     NASD30_360 = "NASD30/360"
 
-    Thirty360 = "30/360" # -> Thirty360:
+    Thirty360 = "30/360"  # -> Thirty360:
     Thirty360_US = "30/360 US"  # -> Thirty360:
     ThirtyE360 = "30E/360"  # -> Thirty360:
 
@@ -46,15 +48,16 @@ class DayCountConv:
             dcf = DayCountConv.NASD30_360
         elif dayCountStr == "30/360 US":
             dcf = DayCountConv.Thirty360_US
-        return	dcf
+        return dcf
 
 
 class CalendarFactory:
     def __init__(self):
-        self.map ={"USA": "UnitedStates","FRA": "France","GRE": "Greece","GBR": "UnitedKingdom"}
+        self.map = {"USA": "UnitedStates", "FRA": "France", "GRE": "Greece", "GBR": "UnitedKingdom"}
 
     def get(self, countryISO):
         return eval("%s()" % self.map[countryISO])
+
 
 class HolidayCities:
     USA = "NY"
@@ -65,6 +68,7 @@ class HolidayCities:
     TARGET = "TARGET"
     ISR = "ISRAEL"
     CHINA = "BEJ"
+
 
 class Currency:
     EUR = "EUR"
@@ -90,6 +94,7 @@ class Currency:
 class TradeSide:
     Bid = "B"
     Ask = "A"
+
 
 class Frequency:
     MONTHLY = "M"
@@ -118,7 +123,7 @@ class Frequency:
 
     @staticmethod
     def getFrequencyNumber(freqStr):
-        freqNumberOut = 1 #<-- default to annual
+        freqNumberOut = 1  # <-- default to annual
         if freqStr == Frequency.MONTHLY:
             freqNumberOut = 12
         elif freqStr == Frequency.DAILY:
@@ -132,6 +137,7 @@ class Frequency:
         elif freqStr == Frequency.ANNUAL:
             freqNumberOut = 1
         return freqNumberOut
+
     try:
         import QuantLib as ql
         @staticmethod
@@ -153,6 +159,7 @@ class Frequency:
     except:
         print("Didn't find Quantlib, so can't define Frequency.getQLFrequency")
 
+
 class ProductClass:
     GovtBond = 1
     CorpBond = 2
@@ -160,3 +167,4 @@ class ProductClass:
     FXSpot = 4
     FXForwards = 5
     FXNDF = 6
+    BondFutures = 7
