f5ad4c5ca28cd4faf6f701b13f7c749bb3d2b38b
Egor Kraev <egor.kraev@gmail.com>
2017-04-06 14:12:52 +0100

    Unit test and example notebook showing how to run notebooks from straight Python

--- mosaicsmartdata/unit_tests/test_run_notebooks.py
+++ mosaicsmartdata/unit_tests/test_run_notebooks.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 import nbformat
+import os
 from nbconvert.preprocessors import ExecutePreprocessor
 from nbconvert.preprocessors.execute import CellExecutionError
 
@@ -7,16 +8,17 @@ from nbconvert.preprocessors.execute import CellExecutionError
 class TestMarkoutMessage(TestCase):
     # Test for Sell in bps over intra-day to COB1
     def test_case_1(self):
-
-        nb = nbformat.read(open('../notebooks/Test_run.ipynb'), as_version=4)
-        ep = ExecutePreprocessor(timeout=600, kernel_name='py35')
+        cwd = os.getcwd()
+        print('***', cwd, '***')
+        nb = nbformat.read(open('.\\msq-domain\\mosaicsmartdata\\unit_tests\\Test_run.ipynb'), as_version=4)
+        ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
 
         try:
-            out = ep.preprocess(nb, {'metadata': {'path': '../notebooks'}})
+            out = ep.preprocess(nb, {'metadata': {'path': '.\\msq-domain\\mosaicsmartdata\\unit_tests'}})
         except CellExecutionError:
             msg = 'Error executing the notebook "%s".\n\n'
             msg += 'See notebook "%s" for the traceback.'
             print(msg)
             raise
         finally:
-            nbformat.write(nb, open('../notebooks/Test_run_result.ipynb', mode='wt'))
+            nbformat.write(nb, open('.\\msq-domain\\mosaicsmartdata\\unit_tests\\Test_run_result.ipynb', mode='wt'))
