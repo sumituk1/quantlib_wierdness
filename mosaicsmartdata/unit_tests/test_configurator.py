6b97266f19c1f410ec16185db00168d533f7e084
Sumit Sengupta <sumit@mosaicsmartdata.com>
2017-04-06 14:31:18 +0100

    configuration logic for walking the stack

--- mosaicsmartdata/unit_tests/test_configurator.py
+++ mosaicsmartdata/unit_tests/test_configurator.py
@@ -33,3 +33,12 @@ class TestConfigurator(TestCase):
         hedge_sym_arr = zz[str([x for x in tenors_list if tenor >= x][-1]).rstrip('0').rstrip('.')]
         hedge_sym_arr = hedge_sym_arr.split(',')
         self.assertEquals(hedge_sym_arr, ['US10YT=RR', 'US30YT=RR'], msg=None)
+
+    def test_case_3(self):
+        config_2 = Configurator('config')
+        zz = config_2.get_section_given_item_val("IT", "GovtBond_listed_Hedge_Mapper")
+        self.assertEquals(zz,'EGB_peri_GovtBond_LISTED_Hedge_Mapper', msg=None)
+        zz = config_2.get_section_given_item_val("IT", "GovtBond_cash_Hedge_Mapper")
+        self.assertEquals(zz, None, msg=None)
+        zz = config_2.get_section_given_item_val("IT", "GovtBond_OTC_Hedge_Mapper")
+        self.assertEquals(zz, 'EGB_peri_GovtBond_OTC_Hedge_Mapper', msg=None)
