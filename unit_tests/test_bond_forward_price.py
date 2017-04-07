edf05015441675a13931972d2606ed8d16518678
Sumit Sengupta <sumit@mosaicsmartdata.com>
2017-04-07 18:29:50 +0100

    repo logic

--- unit_tests/test_bond_forward_price.py
+++ unit_tests/test_bond_forward_price.py
@@ -1,95 +1,119 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.dirname(__file__) + '/' + '../..'))
-
 import unittest
-from risk.bond_mod_duration import *
-from risk.bond_forward_price import *
+from mosaicsmartdata.common.quantlib.bond.bond_mod_duration import *
+from mosaicsmartdata.common.quantlib.bond.bond_forward_price import *
 import numpy as np
-from risk.bond_ytm import bond_ytm
-from risk.bond_price import bond_price
-from common.constants import DayCountConv, CalendarFactory, Frequency
+from mosaicsmartdata.common.quantlib.bond.bond_ytm import *
+from mosaicsmartdata.common.quantlib.bond.bond_price import *
+from mosaicsmartdata.common.constants import DayCountConv, CalendarFactory, Frequency
+from mosaicsmartdata.core.repo_singleton import *
 from datetime import datetime
-from risk.schedule import CSchedule
+from mosaicsmartdata.common.schedule import CSchedule
+
 
 class TestBondForwardPrice(unittest.TestCase):
-
     def test_forward_UST5y_without_coupon(self):
         TOLERANCE = 1e-2
 
         # ==== input =====
+        repo_rate = RepoSingleton()
         price = 99.97656250  # spot settle clean price
-        spot_settle_date = "2016-08-09"  # settle date
+        settle_date = dt.datetime(2016, 8, 9)  # settle date
+        issue_date = dt.datetime(2014, 1, 31)  # -> python datetime
+        maturity_date = dt.datetime(2021, 1, 31)
         coupon = 1.125  # bond coupon
-        frequency = "S"  # coupon frequency
-        next_coupon_date = "2017-01-31"  # next coupon date
-        prev_coupon_date = "2016-07-31"  # previous coupon date
-        forward_date = "2016-08-16"  # forward settle date
-        daycount = "ACT/ACT"  # daycount convention
-        repo = +0.49  # repo rate in %
+        holidayCities = HolidayCities.USA
+        frequency = Frequency.SEMI  # coupon frequency
+        # next_coupon_date = dt.datetime(2017,1,31)
+        # next coupon date (This is needed especially when there is a big short stub)
+        next_coupon_date = bond.getNextCouponDate(issue_date=issue_date,
+                                                  maturity_date=maturity_date,
+                                                  frequency=frequency,
+                                                  holidayCities=holidayCities,
+                                                  settle_date=settle_date)
 
-        forward_price = govbondforwardprice(price, spot_settle_date, prev_coupon_date, next_coupon_date,
-                                            forward_date, coupon, frequency, repo)
+        prev_coupon_date = bond.getLastCouponDate(issue_date=issue_date,
+                                                  maturity_date=maturity_date,
+                                                  frequency=frequency,
+                                                  holidayCities=holidayCities,
+                                                  settle_date=settle_date)
 
-        self.assertLessEqual(np.abs(forward_price - 99.964691)/99.964691, TOLERANCE, msg=None)
+        # prev_coupon_date = dt.datetime(2016, 7, 31)  # previous coupon date
+        forward_date = dt.datetime(2016, 8, 16)  # forward settle date
+        day_count = DayCountConv.ACT_ACT  # daycount convention
+        repo = repo_rate(ccy=Currency.USD, date=settle_date)  # repo rate in %
 
-    def test_forward_UST5y_with_coupon(self):
-        TOLERANCE = 1e-2
+        forward_price = govbond_forwardprice(price=price,
+                                             spot_settle_date=settle_date,
+                                             prev_coupon_date=prev_coupon_date,
+                                             next_coupon_date=next_coupon_date,
+                                             forward_date=forward_date,
+                                             day_count=day_count,
+                                             coupon=coupon,
+                                             frequency=frequency,
+                                             repo=repo)
 
-        # ==== input ===== EMC 2.65 06/01/20 Corp
-        price = 98.823000  # spot settle clean price
-        spot_settle_date = "2015-11-25"  # settle date
-        coupon = 2.65  # bond coupon
-        frequency = "S"  # coupon frequency
-        next_coupon_date = "2016-12-01"  # next coupon date
-        prev_coupon_date = "2015-06-01"  # previous coupon date
-        forward_date = "2015-12-04"  # forward settle date
-        daycount = "ACT/ACT"  # daycount convention
-        repo = +0.49  # repo rate in %
+        self.assertLessEqual(np.abs(forward_price - 99.964691) / 99.964691, TOLERANCE, msg=None)
 
-        forward_price = govbondforwardprice(price, spot_settle_date, prev_coupon_date, next_coupon_date,
-                                            forward_date, coupon, frequency, repo)
-
-        print("Forward price for US 5y with coupon =", forward_price)
-        self.assertLessEqual(np.abs(forward_price - 98.768959) / 98.768959, TOLERANCE, msg=None)
 
     def test_duration_DE15y_without_coupon(self):
         TOLERANCE = 1e-2
 
         # ==== input =====
-        price = 178.255000  # spot settle clean price
-        spot_settle_date = "2016-08-10"  # settle date
-        coupon = 5.5 # bond coupon
-        frequency = "A"  # coupon frequency
-        next_coupon_date = "2017-01-04"  # next coupon date
-        prev_coupon_date = "2016-01-04"  # previous coupon date
-        forward_date = "2016-08-17"  # forward settle date
-        daycount = "ACT/ACT"  # daycount convention
-        repo = -0.298  # repo rate in %
+        repo_rate = RepoSingleton()
+        price = 167.135  # spot settle clean price
 
-        forward_price = govbondforwardprice(price, spot_settle_date, prev_coupon_date, next_coupon_date,
-                                            forward_date, coupon, frequency, repo)
+        coupon = 5.5  # bond coupon
+        frequency = Frequency.ANNUAL  # coupon frequency
+        settle_date = dt.datetime(2017, 4, 11)  # settle date
+        issue_date = dt.datetime(2000, 10, 27)  # -> python datetime
+        maturity_date = dt.datetime(2031, 1, 4)
+        forward_date = dt.datetime(2017,4,14)  # forward settle date
+        day_count = DayCountConv.ACT_ACT  # daycount convention
+        repo = repo_rate(ccy=Currency.EUR, date=settle_date)  # repo rate in %
+        holidayCities=HolidayCities.EUR
+
+        next_coupon_date = bond.getNextCouponDate(issue_date=issue_date,
+                                                  maturity_date=maturity_date,
+                                                  frequency=frequency,
+                                                  holidayCities=holidayCities,
+                                                  settle_date=settle_date)
+
+        prev_coupon_date = bond.getLastCouponDate(issue_date=issue_date,
+                                                  maturity_date=maturity_date,
+                                                  frequency=frequency,
+                                                  holidayCities=holidayCities,
+                                                  settle_date=settle_date)
+
+        forward_price = govbond_forwardprice(price=price,
+                                             spot_settle_date=settle_date,
+                                             prev_coupon_date=prev_coupon_date,
+                                             next_coupon_date=next_coupon_date,
+                                             forward_date=forward_date,
+                                             day_count=day_count,
+                                             coupon=coupon,
+                                             frequency=frequency,
+                                             repo=repo)
 
         print("Forward price for DE 15y without coupon =", forward_price)
-        self.assertLessEqual(np.abs(forward_price - 178.139289) / 178.139289, TOLERANCE, msg=None)
+        self.assertLessEqual(np.abs(forward_price - 167.0145) / 167.0145, TOLERANCE, msg=None)
 
-    def test_duration_DE15y_with_coupon(self):
-        TOLERANCE = 1e-2
-        # ==== input =====
-        price = 114.745000  # spot settle clean price
-        spot_settle_date = "2015-10-20"  # settle date
-        coupon = 1.625  # bond coupon
-        frequency = "A"  # coupon frequency
-        next_coupon_date = "2015-10-24"  # next coupon date
-        prev_coupon_date = "2014-10-24"  # previous coupon date
-        forward_date = "2015-10-28"  # forward settle date
-        daycount = "ACT/ACT"  # daycount convention
-        repo = -0.298  # repo rate in %
-
-        forward_price = govbondforwardprice(price, spot_settle_date, prev_coupon_date, next_coupon_date,
-                                            forward_date, coupon, frequency, repo)
-        print("Forward price for DE 15y with coupon =", forward_price)
-        self.assertLessEqual(np.abs(forward_price - 114.701754) / 114.701754, TOLERANCE, msg=None)
+    # def test_duration_DE15y_with_coupon(self):
+    #     TOLERANCE = 1e-2
+    #     # ==== input =====
+    #     price = 114.745000  # spot settle clean price
+    #     spot_settle_date = "2015-10-20"  # settle date
+    #     coupon = 1.625  # bond coupon
+    #     frequency = "A"  # coupon frequency
+    #     next_coupon_date = "2015-10-24"  # next coupon date
+    #     prev_coupon_date = "2014-10-24"  # previous coupon date
+    #     forward_date = "2015-10-28"  # forward settle date
+    #     daycount = "ACT/ACT"  # daycount convention
+    #     repo = -0.298  # repo rate in %
+    #
+    #     forward_price = govbond_forwardprice(price, spot_settle_date, prev_coupon_date, next_coupon_date,
+    #                                         forward_date, coupon, frequency, repo)
+    #     print("Forward price for DE 15y with coupon =", forward_price)
+    #     self.assertLessEqual(np.abs(forward_price - 114.701754) / 114.701754, TOLERANCE, msg=None)
 
 
 if __name__ == '__main__':
