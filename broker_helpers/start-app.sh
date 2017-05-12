6c9a0775b13cdafab8152dfe177294b605824e85
James Clinton <james.clinton@avexa.co.uk>
2017-05-12 14:03:20 +0100

    Topics created

--- broker_helpers/start-app.sh
+++ broker_helpers/start-app.sh
@@ -5,4 +5,11 @@ pwd
 ./broker_helpers/wait-for-it.sh -t 120 kafka:9092
 result=$?
 
+cd $KAFKA_HOME
+
+./bin/kafka-topics.sh --zookeeper zookeeper --create --topic topic-a --partitions 1 --replication-factor 1
+./bin/kafka-topics.sh --zookeeper zookeeper --create --topic topic-b --partitions 1 --replication-factor 1
+
+sleep 10
+
 python /scripts/start-app.py
